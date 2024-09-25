dd8bf50a6ffe37a26aee49baa030634a8e5d7bfa
pyth0g <lenart.kladnik@gmail.com>
2024-09-25 15:51:02 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// document.cookie = "darkMode=1; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
-
 function toggleMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
